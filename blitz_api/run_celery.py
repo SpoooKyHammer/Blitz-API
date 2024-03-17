3cd2fd193d8da97b3f03ddc36e8dc887b10a27d5
SpoooKyHammer <spookyhammer2300@gmail.com>
2024-03-17 13:27:20 +0530

    moved celery endpoint

---
+++ blitz_api/run_celery.py
@@ -0,0 +1,7 @@
+
+"""Entry point to create celery application."""
+
+from blitz_api import create_app
+
+flask_app = create_app()
+celery_app =  flask_app.extensions["celery"]
--- make_celery.py
+++
@@ -1,6 +0,0 @@
-"""Entry point to create celery application."""
-
-from blitz_api import create_app
-
-flask_app = create_app()
-celery_app =  flask_app.extensions["celery"]
